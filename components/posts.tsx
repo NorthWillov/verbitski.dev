ed42de22fd8052ba844a9867a29321d2c063bf14
NorthWillov <northwillov@gmail.com>
2021-12-07 20:00:15 +0100

    Add test project

--- components/posts.tsx
+++ components/posts.tsx
@@ -8,9 +8,7 @@ type Props = {
 const Posts = ({ posts }: Props) => {
   return (
     <section>
-      <h2 className="mb-8 text-6xl md:text-7xl font-bold tracking-tighter leading-tight">
-        Blog
-      </h2>
+      <h1 className="text-3xl text-center my-10">Blogs</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-16 lg:gap-x-32 gap-y-20 md:gap-y-32 mb-32">
         {posts.map((post) => (
           <PostPreview
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -2,6 +2,7 @@ import React, { FC } from "react"
 import Container from "../components/container"
 import Layout from "../components/layout"
 import Head from "next/head"
+import Link from "next/link"
 import Navbar from "../components/navbar"
 
 const Projects: FC = () => {
@@ -12,7 +13,31 @@ const Projects: FC = () => {
       </Head>
       <Container>
         <Navbar />
-        <h1>Projects:</h1>
+        <h1 className="text-3xl text-center my-10">Projects</h1>
+        <div className="w-64 border border-black rounded">
+          <img
+            className=""
+            src="https://i.ibb.co/YB36KZc/pizza-wege.png"
+            alt="project"
+          />
+          <h2 className="text-center text-xl"> Pizzeria 'Riccardo' 🍕</h2>
+          <p className="text-center">
+            Full Stack Web Application for food delivery from "Pizzeria &
+            Restaurant - Riccardo" 👨‍🍳
+          </p>
+          <div className="flex justify-around">
+            <Link href="https://riccardopizzeria.herokuapp.com">
+              <a className="border border-black rounded p-1" target="_blank">
+                <i className="fas fa-play"></i> Demo
+              </a>
+            </Link>
+            <Link href="https://github.com/NorthWillov/food_service">
+              <a className="border border-black rounded p-1" target="_blank">
+                <i className="fab fa-github"></i> Source
+              </a>
+            </Link>
+          </div>
+        </div>
       </Container>
     </Layout>
   )
