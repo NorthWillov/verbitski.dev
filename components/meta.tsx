6aea9c002b8db04dd9d3e5e97d036708df9206d6
NorthWillov <northwillov@gmail.com>
2021-12-07 14:55:55 +0100

    Add icons

--- components/meta.tsx
+++ components/meta.tsx
@@ -1,5 +1,5 @@
-import Head from 'next/head'
-import { CMS_NAME, HOME_OG_IMAGE_URL } from '../lib/constants'
+import Head from "next/head"
+import { CMS_NAME, HOME_OG_IMAGE_URL } from "../lib/constants"
 
 const Meta = () => {
   return (
@@ -28,6 +28,13 @@ const Meta = () => {
         color="#000000"
       />
       <link rel="shortcut icon" href="/favicon/favicon.ico" />
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
+        integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA=="
+        crossorigin="anonymous"
+        referrerpolicy="no-referrer"
+      />
       <meta name="msapplication-TileColor" content="#000000" />
       <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
       <meta name="theme-color" content="#000" />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Container from "../components/container"
 import Layout from "../components/layout"
 import Navbar from "../components/navbar"
+import Link from "next/link"
 import Head from "next/head"
 import { FC } from "react"
 
@@ -19,16 +20,36 @@ const Index: FC = () => {
               srcSet="/assets/blog/authors/artem.jpeg"
               alt="Artem"
             />
-            <p className="w-1/2">
-              Hi there 👋 <br />I am a Front-End Developer with hands-on to
-              build user-friendly and professional looking sites. I have
-              knowledge to solve a problem across many mediums and especially
-              adept at taking an already existed project to the next level. With
-              every project I aim to make the user experience intuitive and easy
-              to follow for any level of user. I look forward to using my skills
-              as a developer and designer as part of a quality-driven team to
-              build easy-to-use and professional looking sites.
-            </p>
+            <div className="w-1/2">
+              <p>
+                <b>Hi there</b> 👋 <br />I am a Software Developer with hands-on
+                to build user-friendly and professional looking sites. I have
+                knowledge to solve a problem across many mediums and especially
+                adept at taking an already existed project to the next level.
+                With every project I aim to make the user experience intuitive
+                and easy to follow for any level of user. I look forward to
+                using my skills as a developer and designer as part of a
+                quality-driven team to build easy-to-use and professional
+                looking sites.
+              </p>
+              <div className="mt-3">
+                <Link href="https://github.com/NorthWillov">
+                  <a target="_blank">
+                    <i className="fab fa-github text-4xl hover:text-green-700 cursor-pointer mr-3"></i>
+                  </a>
+                </Link>
+                <Link href="https://www.linkedin.com/in/artemverbytskyi">
+                  <a target="_blank">
+                    <i className="fab fa-linkedin text-4xl hover:text-green-700 cursor-pointer mr-3"></i>
+                  </a>
+                </Link>
+                <Link href="https://www.instagram.com/artemverbytskyi">
+                  <a target="_blank">
+                    <i className="fab fa-instagram text-4xl hover:text-green-700 cursor-pointer mr-3"></i>
+                  </a>
+                </Link>
+              </div>
+            </div>
           </div>
         </Container>
       </Layout>
