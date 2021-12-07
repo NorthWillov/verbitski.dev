a91b6f5d18135c64325307f7d0c46a6ac4e5a06a
NorthWillov <northwillov@gmail.com>
2021-12-07 16:05:20 +0100

    Fix cover image wrong href

--- components/cover-image.tsx
+++ components/cover-image.tsx
@@ -1,5 +1,5 @@
-import cn from 'classnames'
-import Link from 'next/link'
+import cn from "classnames"
+import Link from "next/link"
 
 type Props = {
   title: string
@@ -12,15 +12,15 @@ const CoverImage = ({ title, src, slug }: Props) => {
     <img
       src={src}
       alt={`Cover Image for ${title}`}
-      className={cn('shadow-small', {
-        'hover:shadow-medium transition-shadow duration-200': slug,
+      className={cn("shadow-small", {
+        "hover:shadow-medium transition-shadow duration-200": slug,
       })}
     />
   )
   return (
     <div className="sm:mx-0">
       {slug ? (
-        <Link as={`/posts/${slug}`} href="/posts/[slug]">
+        <Link as={`/blog/${slug}`} href="/blog/[slug]">
           <a aria-label={title}>{image}</a>
         </Link>
       ) : (
