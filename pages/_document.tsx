45dc6f5c20f66fc9708b2ded7919d2d0f25659b8
NorthWillov <northwillov@gmail.com>
2022-01-18 20:18:40 +0100

    Test dark theme

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,11 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from "next/document"
 
 export default class MyDocument extends Document {
   render() {
     return (
-      <Html lang="en">
+      <Html className="dark" lang="en">
         <Head />
-        <body>
+        <body className="dark:bg-gray-900">
           <Main />
           <NextScript />
         </body>
