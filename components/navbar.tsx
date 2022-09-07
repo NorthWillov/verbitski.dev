51e07696979b8c41761f09dc6de2ea2c55c4e2b8
NorthWillov <northwillov@gmail.com>
2022-09-07 23:00:35 +0200

    Add burger btn

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,21 +1,105 @@
 import Link from "next/link";
+import { useState } from "react";
 import { useRouter } from "next/router";
 import Container from "./container";
 
 export const Navbar = () => {
+  const [isNavOpen, setIsNavOpen] = useState(false);
   const router = useRouter();
 
   return (
-    <nav className="bg-accent-1 text-white py-5 fixed top-0 left-0 right-0 z-30">
+    <div className="bg-accent-1 text-white py-5 fixed top-0 left-0 right-0 z-30">
       <Container>
         <div className="flex justify-between">
           <Link href="/">
-            <a className="text-xl font-bold">VERBITSKI</a>
+            <a className="text-xl font-bold ml-5 md:ml-0">VERBITSKI</a>
           </Link>
-          <div>
-            <ul className="flex">
+          <nav className="md:flex items-center space-x-8">
+            {/* MOBILE SECTION */}
+
+            <section className="flex md:hidden">
+              <div
+                className="space-y-2 mr-10 mt-1"
+                onClick={() => setIsNavOpen((prev) => !prev)} // toggle isNavOpen state on click
+              >
+                <span className="block h-0.5 w-8 animate-pulse bg-white"></span>
+                <span className="block h-0.5 w-8 animate-pulse bg-white"></span>
+                <span className="block h-0.5 w-8 animate-pulse bg-white"></span>
+              </div>
+
+              <div className={isNavOpen ? "showMenuNav" : "hideMenuNav"}>
+                <div
+                  className="absolute top-0 right-0 px-8 py-8"
+                  onClick={() => setIsNavOpen(false)} // change isNavOpen state to false to close the menu
+                >
+                  <svg
+                    className="h-8 w-8 text-white"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  >
+                    <line x1="18" y1="6" x2="6" y2="18" />
+                    <line x1="6" y1="6" x2="18" y2="18" />
+                  </svg>
+                </div>
+                <ul className="flex flex-col items-center justify-between min-h-[250px]">
+                  <li
+                    className={`${
+                      router.pathname == "/" ? "underline" : ""
+                    } burger-item`}
+                  >
+                    <Link href="/">
+                      <a>HOME</a>
+                    </Link>
+                  </li>
+                  <li
+                    className={`${
+                      router.pathname == "/about" ? "underline" : ""
+                    } burger-item`}
+                  >
+                    <Link href="/about">
+                      <a>ABOUT</a>
+                    </Link>
+                  </li>
+                  <li
+                    className={`${
+                      router.pathname == "/blog" ? "underline" : ""
+                    } burger-item`}
+                  >
+                    <Link href="/blog">
+                      <a>BLOG</a>
+                    </Link>
+                  </li>
+                  <li
+                    className={`${
+                      router.pathname == "/projects" ? "underline" : ""
+                    } burger-item`}
+                  >
+                    <Link href="/projects">
+                      <a>PROJECTS</a>
+                    </Link>
+                  </li>
+                  <li
+                    className={`${
+                      router.pathname == "/contact" ? "underline" : ""
+                    } burger-item`}
+                  >
+                    <Link href="/contact">
+                      <a>CONTACT</a>
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </section>
+
+            {/* DESCTOP SECTION */}
+
+            <ul className="hidden space-x-8 md:flex">
               <li
-                className={`${router.pathname == "/" ? "underline" : ""} pr-6`}
+                className={`${router.pathname == "/" ? "underline" : ""}`}
               >
                 <Link href="/">
                   <a>HOME</a>
@@ -24,7 +108,7 @@ export const Navbar = () => {
               <li
                 className={`${
                   router.pathname == "/about" ? "underline" : ""
-                } pr-6`}
+                }`}
               >
                 <Link href="/about">
                   <a>ABOUT</a>
@@ -33,7 +117,7 @@ export const Navbar = () => {
               <li
                 className={`${
                   router.pathname == "/blog" ? "underline" : ""
-                } pr-6`}
+                }`}
               >
                 <Link href="/blog">
                   <a>BLOG</a>
@@ -42,7 +126,7 @@ export const Navbar = () => {
               <li
                 className={`${
                   router.pathname == "/projects" ? "underline" : ""
-                } pr-6`}
+                }`}
               >
                 <Link href="/projects">
                   <a>PROJECTS</a>
@@ -58,10 +142,10 @@ export const Navbar = () => {
                 </Link>
               </li>
             </ul>
-          </div>
+          </nav>
         </div>
       </Container>
-    </nav>
+    </div>
   );
 };
 
