00969bceb0bffef0507bbd1f7ee7f3774b425d94
NorthWillov <northwillov@gmail.com>
2026-03-11 13:22:58 +0100

    remove: console message

--- components/contact-form.tsx
+++ components/contact-form.tsx
@@ -15,8 +15,6 @@ const ContactForm = () => {
   const [error, setError] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
-  console.log(process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY)
-
   const handleSubmit = (e: any) => {
     e.preventDefault();
     setIsLoading(true);
